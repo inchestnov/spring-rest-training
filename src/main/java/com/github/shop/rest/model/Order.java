d86336773dc2437a20d8a5610803a013c49116ac
igorchestnov <chestnov@haulmont.com>
2021-01-09 20:12:17 +0400

    Optimize import in project

--- src/main/java/com/github/shop/rest/model/Order.java
+++ src/main/java/com/github/shop/rest/model/Order.java
@@ -1,7 +1,10 @@
 package com.github.shop.rest.model;
 
 import com.github.inchestnov.generatorstarter.Generated;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
